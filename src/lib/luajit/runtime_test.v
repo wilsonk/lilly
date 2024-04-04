952ac8f7edb2f87c64cc37a69a24e298b8246885
tauraamui <adampstringer@protonmail.com>
2024-04-04 13:01:35 +0100

    test: verify that the actual real config scenario can work

--- src/lib/luajit/runtime_test.v
+++ src/lib/luajit/runtime_test.v
@@ -2,10 +2,26 @@ module luajit
 
 struct Test{
 	value int
-	name string
+	name  string
+	extra int
 }
 
 fn test_parse() {
 	c := parse[Test]("config = { value = 43384, name = 'thing' }") or { panic(err) }
 	assert c == Test{ value: 43384, name: "thing" }
+}
+
+struct Config {
+	leader_key             string
+	relative_line_numbers  int
+	insert_tabs_not_spaces int
+}
+
+fn test_parse_config_replacement() {
+	config_in_lua := "config = { leader_key = ';', relative_line_numbers = 1, insert_tabs_not_spaces = 1 }"
+	parsed_config := parse[Config](config_in_lua) or { panic(err) }
+	assert parsed_config == Config {
+		leader_key: ";", relative_line_numbers: 1, insert_tabs_not_spaces: 1
+	}
+
 }
